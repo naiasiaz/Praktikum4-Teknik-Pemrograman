e529f99b2acd7fe17b2ac23682ed2480d83d1bc4
naiasiaz <naiasitiazzahra27@gmail.com>
2023-02-10 20:48:27 +0700

    Build and import jar file

---
+++ W4-JarFile/DemoWeb/src/com/demo/Runner.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import com.polban.jtk.Calculator;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		
+		System.out.println(c.add(2, 5));
+
+	}
+
+}
---
+++ W4-JarFile/NaiaLib/src/com/polban/jtk/Calculator.java
@@ -0,0 +1,7 @@
+package com.polban.jtk;
+
+public class Calculator {
+	public int add(int i, int j) {
+		return i+j;
+	}
+}
